bccd50a5c0779d9fe5569c14b79d8a0d1f7ed66e
TXMack713 <49073356+TXMack713@users.noreply.github.com>
2022-10-23 22:27:06 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+let compChoice, userChoice;
+
+function getComputerChoice() {
+    return Math.floor(Math.random()*(3-1+1)+1);
+}
+
+compChoice = getComputerChoice();
+
+let entry;      // Placeholder to verify proper selection was made
+console.info("Welcome to Rock-Paper-Scissors");
+entry = prompt("Please make your selection: 1 for Rock, 2 for Paper, or 3 for Scissors");
+

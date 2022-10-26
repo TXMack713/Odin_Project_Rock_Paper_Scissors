c183d42923a3f4d28e1ca0882c4735de55fbc027
Anthony <49073356+TXMack713@users.noreply.github.com>
2022-10-25 21:53:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,9 +20,13 @@ function getUserChoice() {
   
   check = parseInt(entry);
   
-  if ((entry < 1) || (entry > 3) || ((typeof check) !== 'number') || (check == "undefined")) {
+  while ((entry < 1) || (entry > 3) || ((typeof check) !== 'number') || (check == "undefined")) {
     alert('Invalid entry');
-    getUserChoice(); 
+    entry = prompt(
+    'Please make your selection: 1 for Rock, 2 for Paper, or 3 for Scissors'
+  );
+  
+  check = parseInt(entry);
   }
   return parseInt(entry);
 }

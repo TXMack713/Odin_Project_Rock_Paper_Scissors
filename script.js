62e8a8aa55d5f3f86f9f0a4635dcfe6c86287029
Anthony Mack <mackattack713@gmail.com>
2023-01-11 09:02:53 -0600

    Queried the buttons to begin playing the game.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let compChoice = '', userChoice = '';
+let compChoice = "",
+  userChoice = "";
 
-console.log('Welcome to Rock-Paper-Scissors!');
-console.log('First to 5 victories wins the tournament.');
+console.log("Welcome to Rock-Paper-Scissors!");
+console.log("First to 5 victories wins the tournament.");
 
 let compWins = 0,
   userWins = 0,
@@ -13,15 +14,18 @@ function getComputerChoice() {
 
 function getUserChoice() {
   let entry; // Placeholder to verify proper selection was made
-  entry = prompt(
-    'Please make your selection: 1 for Rock, 2 for Paper, or 3 for Scissors'
-  );
+  const rock = document.body.getElementById("rock");
+  const paper = document.body.getElementById("paper");
+  const scissors = document.body.getElementById("scissors");
+  // entry = prompt(
+  //   'Please make your selection: 1 for Rock, 2 for Paper, or 3 for Scissors'
+  // );
 
-  while(entry < 1 || entry > 3 || (typeof entry != 'number')) {
-    entry = prompt(
-      "Please enter a number for your selection: 1 for Rock, 2 for Paper, or 3 for Scissors:"
-    );
-  }
+  // while(entry < 1 || entry > 3 || (typeof entry != 'number')) {
+  //   entry = prompt(
+  //     "Please enter a number for your selection: 1 for Rock, 2 for Paper, or 3 for Scissors:"
+  //   );
+  // }
 
   return entry;
 }
@@ -30,15 +34,15 @@ function compareChoices(machine, user) {
   if (machine == 1) {
     switch (user) {
       case 1:
-        console.log('Draw, Rock vs. Rock');
+        console.log("Draw, Rock vs. Rock");
         draws++;
         break;
       case 2:
-        console.log('You win! Paper covers Rock!');
+        console.log("You win! Paper covers Rock!");
         userWins++;
         break;
       case 3:
-        console.log('You lose! Rock breaks Scissors!');
+        console.log("You lose! Rock breaks Scissors!");
         compWins++;
         break;
     }
@@ -47,15 +51,15 @@ function compareChoices(machine, user) {
   if (machine == 2) {
     switch (user) {
       case 1:
-        console.log('You lose! Paper covers Rock!');
+        console.log("You lose! Paper covers Rock!");
         compWins++;
         break;
       case 2:
-        console.log('Draw, Paper vs. Paper');
+        console.log("Draw, Paper vs. Paper");
         draws++;
         break;
       case 3:
-        console.log('You win! Scissors cut Paper!');
+        console.log("You win! Scissors cut Paper!");
         userWins++;
         break;
     }
@@ -64,15 +68,15 @@ function compareChoices(machine, user) {
   if (machine == 3) {
     switch (user) {
       case 1:
-        console.log('You win! Rock breaks Scissors!');
+        console.log("You win! Rock breaks Scissors!");
         userWins++;
         break;
       case 2:
-        console.log('You lose! Scissors cut Paper!');
+        console.log("You lose! Scissors cut Paper!");
         compWins++;
         break;
       case 3:
-        console.log('Draw, Scissors vs Scissors');
+        console.log("Draw, Scissors vs Scissors");
         draws++;
         break;
     }
@@ -85,8 +89,8 @@ while (userWins != 5 || compWins != 5) {
   compareChoices(compChoice, userChoice);
 }
 
-if(userWins == 5) {
+if (userWins == 5) {
   console.log("Congratulations! You won!");
-} else if(compWins == 5) {
+} else if (compWins == 5) {
   console.log("The computer won, better luck next time.");
 }

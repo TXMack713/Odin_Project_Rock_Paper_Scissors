5aac6c7616446e861ad03bd8bedb5dc72a7f35ee
Anthony Mack <mackattack713@gmail.com>
2023-01-13 09:28:15 -0600

    Updated the CSS styling and game logic

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-let compChoice = '',
-  userChoice = '';
-
-console.log('Welcome to Rock-Paper-Scissors!');
-console.log('First to 5 victories wins the tournament.');
+let compChoice = "",
+  userChoice = "";
 
 let compWins = 0,
   userWins = 0,
@@ -11,46 +8,27 @@ let compWins = 0,
 const userResults = document.getElementById("userResults");
 const computerResults = document.getElementById("computerResults");
 
-function getComputerChoice() {
-  return Math.floor(Math.random() * (3 - 1 + 1) + 1);
-}
+userResults.textContent = userWins;
+computerResults.textContent = compWins;
 
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
-rock.addEventListener("click", () => {
-  compareChoices(getComputerChoice(), 1);
-});
+rock.addEventListener("click", getComputerChoice(1));
 
-paper.addEventListener("click", () => {
-  compareChoices(getComputerChoice(), 2);
-});
+paper.addEventListener("click", getComputerChoice(2));
 
-scissors.addEventListener("click", () => {
-  compareChoices(getComputerChoice(), 3);
-});
+scissors.addEventListener("click", getComputerChoice(3));
 
-/*function getUserChoice() {
-  let entry; // Placeholder to verify proper selection was made
 
-  entry = prompt(
-    'Please make your selection: 1 for Rock, 2 for Paper, or 3 for Scissors'
-  );
-
-  while(entry < 1 || entry > 3 || (typeof entry != 'number')) {
-    entry = prompt(
-      "Please enter a number for your selection: 1 for Rock, 2 for Paper, or 3 for Scissors:"
-    );
-  }
-
-  return entry;
-}*/
+function getComputerChoice(choice) {
+  const compSelection = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  compareChoices(compSelection, choice);
+}
 
 function compareChoices(machine, user) {
   const p = document.getElementById("outcome");
-  // userResults = document.getElementById("userResults");
-  // computerResults = document.getElementById("computerResults");
 
   if (machine == 1) {
     switch (user) {
@@ -97,41 +75,43 @@ function compareChoices(machine, user) {
         compWins++;
         break;
       case 3:
-        console.log("Draw, Scissors vs Scissors");
+        p.textContent = "Draw, Scissors vs Scissors";
         draws++;
         break;
     }
   }
+
   userResults.textContent = userWins;
   computerResults.textContent = compWins;
-
-  if (userWins == 5 || compWins == 5) {
-    rock.removeEventListener("click");
-    paper.removeEventListener("click");
-    scissors.removeEventListener("click");
-    if (userWins == 5) {
-      alert("Congratulations! You won!");
-    } else if (compWins == 5) {
-      alert("The computer won, better luck next time.");
-  }
-
-  const sect = document.createElement('section');
-  const para = document.createElement('p');
-  para.textContent = "Would you like to play again?";
-
-  const reset = document.createElement('button');
-  reset.textContent = 'RESET';
-  
-  sect.appendChild = para;
-  sect.appendChild = reset;
-
-  const article = document.getElementById('results');
-
-  document.body.insertBefore(sect,article);
 }
 
-/*while (userWins != 5 || compWins != 5) {
-  compChoice = getComputerChoice();
-  userChoice = getUserChoice();
-  compareChoices(compChoice, userChoice);
-}*/
+if (userWins == 5 || compWins == 5) {
+  if (userWins == 5) {
+    alert("Congratulations! You won!");
+  } else if (compWins == 5) {
+    alert("The computer won, better luck next time.");
+  }
+  stop();
+}
+
+function stop() {
+  rock.removeEventListener("click", getComputerChoice(1));
+  paper.removeEventListener("click", getComputerChoice(2));
+  scissors.removeEventListener("click", getComputerChoice(3));
+}
+
+/*
+const sect = document.createElement('section');
+const para = document.createElement('p');
+para.textContent = "Would you like to play again?";
+
+const reset = document.createElement('button');
+reset.textContent = 'RESET';
+
+sect.appendChild = para;
+sect.appendChild = reset;
+
+const article = document.getElementById('results');
+
+document.body.insertBefore(sect,article);
+*/

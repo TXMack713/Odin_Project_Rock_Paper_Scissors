58c850cf61bac3364e565bef3ca528520577f60d
Anthony Mack <mackattack713@gmail.com>
2023-01-25 08:46:39 -0600

    Updated the game logic

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let compChoice = "",
-  userChoice = "";
-
 let compWins = 0,
   userWins = 0,
   draws = 0;
@@ -15,59 +12,55 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
-const instructions = document.querySelector("instructions");
+// const instructions = document.querySelector("instructions");
 
-const playButton = document.createElement("button");
-playButton.textContent = "PLAY!";
+// const outcome = document.getElementById("outcome");
 
-const buttons = document.getElementById("buttons");
-buttons.innerHTML = playButton;
+// /*
+// const playButton = document.createElement("button");
+// playButton.textContent = "PLAY!";
 
-playButton.addEventListener("click", play());
+// const buttons = document.getElementById("buttons");
+// buttons.innerHTML = playButton;
 
-/*
-rock.addEventListener("click", play(1));
+// playButton.addEventListener("click", play());
+// */
 
-paper.addEventListener("click", play(2));
+rock.addEventListener("click", () => {
+  console.log("User picked rock");
+  play(1);
+});
 
-scissors.addEventListener("click", play(3));
-*/
+paper.addEventListener("click", () => {
+  console.log("User picked paper");
+  play(2);
+});
 
-function play() {
-  const rock = document.createElement("button");
-  const paper = document.createElement("paper");
-  const scissors = document.createElement("scissors");
-  
-  playButton.innerHTML = rock+paper+scissors;
-
-  rock.addEventListener("click", play(1));
-  paper.addEventListener("click", play(2));
-  scissors.addEventListener("click", play(3));
-  
-  const compSelection = getComputerChoice();
-  compareChoices(compSelection, selection);
-}
+scissors.addEventListener("click", () => {
+  console.log("User picked scissors");
+  play(3);
+});
 
 function getComputerChoice() {
-  return Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  const choice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+  console.log("Computer choice is: " + choice);
+  return choice;
 }
 
 function compareChoices(machine, user) {
-  const p = document.getElementById("outcome");
-
   if (machine == 1) {
     switch (user) {
       case 1:
-        p.textContent = "Draw, Rock vs. Rock";
+        outcome.textContent = "Draw, Rock vs. Rock";
         ++draws;
         break;
       case 2:
-        p.textContent = "You win! Paper covers Rock!";
+        outcome.textContent = "You win! Paper covers Rock!";
         ++userWins;
         userResults.textContent = userWins;
         break;
       case 3:
-        p.textContent = "You lose! Rock breaks Scissors!";
+        outcome.textContent = "You lose! Rock breaks Scissors!";
         ++compWins;
         computerResults.textContent = compWins;
         break;
@@ -77,16 +70,16 @@ function compareChoices(machine, user) {
   if (machine == 2) {
     switch (user) {
       case 1:
-        p.textContent = "You lose! Paper covers Rock!";
+        outcome.textContent = "You lose! Paper covers Rock!";
         ++compWins;
         computerResults.textContent = compWins;
         break;
       case 2:
-        p.textContent = "Draw, Paper vs. Paper";
+        outcome.textContent = "Draw, Paper vs. Paper";
         ++draws;
         break;
       case 3:
-        p.textContent = "You win! Scissors cut Paper!";
+        outcome.textContent = "You win! Scissors cut Paper!";
         ++userWins;
         userResults.textContent = userWins;
         break;
@@ -96,21 +89,38 @@ function compareChoices(machine, user) {
   if (machine == 3) {
     switch (user) {
       case 1:
-        p.textContent = "You win! Rock breaks Scissors!";
-        ++userWins+
+        outcome.textContent = "You win! Rock breaks Scissors!";
+        ++userWins;
         userResults.textContent = userWins;
         break;
       case 2:
-        p.textContent = "You lose! Scissors cut Paper!";
+        outcome.textContent = "You lose! Scissors cut Paper!";
         ++compWins;
         computerResults.textContent = compWins;
         break;
       case 3:
-        p.textContent = "Draw, Scissors vs Scissors";
+        outcome.textContent = "Draw, Scissors vs Scissors";
         ++draws;
         break;
     }
   }
+}
+
+function play(selection) {
+  // const rock = document.createElement("button");
+  // const paper = document.createElement("paper");
+  // const scissors = document.createElement("scissors");
+
+  // playButton.outerHTML = rock+paper+scissors;
+
+  // rock.addEventListener("click", play(1));
+  // paper.addEventListener("click", play(2));
+  // scissors.addEventListener("click", play(3));
+
+  console.log("A selection was made");
+
+  const compSelection = getComputerChoice();
+  compareChoices(compSelection, selection);
 }
 
 if (userWins == 5 || compWins == 5) {
@@ -124,11 +134,21 @@ if (userWins == 5 || compWins == 5) {
 
 function stop() {
   buttons.innerHTML = playButton;
-  /*
-  rock.removeEventListener("click", play(1));
-  paper.removeEventListener("click", play(2));
-  scissors.removeEventListener("click", play(3));
-  */
+
+  rock.removeEventListener("click", () => {
+    console.log("User picked rock");
+    play(1);
+  });
+
+  paper.removeEventListener("click", () => {
+    console.log("User picked paper");
+    play(2);
+  });
+
+  scissors.removeEventListener("click", () => {
+    console.log("User picked scissors");
+    play(3);
+  });
 }
 
 const sect = document.createElement("section");
@@ -143,11 +163,12 @@ sect.appendChild = reset;
 
 const article = document.getElementById("results");
 
-document.body.insertBefore(sect, article);
+const main = document.getElementsByTagName("main");
+
+main.insertBefore(sect, article);
 
 function gameReset() {
   compWins = 0;
   userWins = 0;
   draws = 0;
-  play();
 }
